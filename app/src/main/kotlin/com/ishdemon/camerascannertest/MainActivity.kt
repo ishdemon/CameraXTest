6edfd6bd857dc6e775b804f4800fed85a90d208f
ishdemon <ishdemon10@gmail.com>
2022-10-03 01:53:23 +0530

    Added CameraActivity

---
+++ app/src/main/kotlin/com/ishdemon/camerascannertest/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.ishdemon.camerascannertest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ishdemon.camerascannertest.theme.CameraTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CameraTestTheme {
+                // A surface container using the 'background' color from the theme
+                HomeScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CameraTestTheme {
+        Greeting("Android")
+    }
+}
